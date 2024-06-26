5c3fc70dc019194292eee088b411147b763e764e
DESKTOP-RMQ5C3B\tokas <tkrshun0408@gmail.com>
2024-06-26 22:12:07 +0900

    BeatmapLoaderクラスを追加

---
+++ Assets/Rhythm/Scripts/Logics/Managers/BeatmapLoader.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Rhythm
+{
+    public static class BeatmapLoader
+    {
+        public static NoteData[] Parse(TextAsset file)
+        {
+            return default;
+        }
+    }
+}
