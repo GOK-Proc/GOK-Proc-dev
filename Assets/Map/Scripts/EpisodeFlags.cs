4fe5a61a357428e350174b141c1e589c33f142b3
kamekiti <ishi.y0331kamekiti@gmail.com>
2024-07-08 18:04:58 +0900

    Episodeフラグをjson形式で保存する機能の追加

--- Assets/Map/Scripts/EpisodeFlags.cs
+++ Assets/Map/Scripts/EpisodeFlags.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 using UnityEngine;
 
 [CreateAssetMenu]
@@ -8,12 +9,20 @@ public class EpisodeFlags : ScriptableObject
 	[SerializeField] private List<EpisodeFlagPair> _flagsList;
 	public List<EpisodeFlagPair> FlagsList { get { return _flagsList; } private set { _flagsList = value; } }
 
-#if UNITY_EDITOR
-	private void OnValidate()
+	private string _path;
+
+	private void OnEnable()
 	{
-		FlagsList = FlagsList.OrderBy(kvp => kvp.Key.Item1).ThenBy(kvp => kvp.Key.Item2).ToList();
+#if UNITY_EDITOR
+		_path = Path.Combine(Application.dataPath, "Map/EpisodeFlags.json");
+#else
+		_path = Path.Combine(Application.persistentDataPath, "EpisodeFlags.json");
+#endif
+
+		LoadJson();
 	}
 
+#if UNITY_EDITOR
 	public void ResetFlags(EpisodeData episodeData)
 	{
 		FlagsList.Clear();
@@ -23,6 +32,32 @@ public class EpisodeFlags : ScriptableObject
 		}
 
 		FlagsList = FlagsList.OrderBy(kvp => kvp.Key.Item1).ThenBy(kvp => kvp.Key.Item2).ToList();
+
+		SaveJson();
 	}
 #endif
+
+	private void SaveJson()
+	{
+		string json = JsonUtility.ToJson(this);
+		using (var writer = new StreamWriter(_path))
+		{
+			writer.Write(json);
+		}
+	}
+
+	private void LoadJson()
+	{
+		if (!File.Exists(_path))
+		{
+			SaveJson();
+			return;
+		}
+
+		using (var reader = new StreamReader(_path))
+		{
+			string json = reader.ReadToEnd();
+			JsonUtility.FromJsonOverwrite(json, this);
+		}
+	}
 }
