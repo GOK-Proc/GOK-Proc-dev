5f2e4062fe4cb8abf9a33a6e9d280893fa1e46f6
Ryotan-proce <ryotan0511@gmail.com>
2024-08-04 00:13:32 +0900

    MusicSelectionのUI要素を定義
    MonoBehaivior継承クラスと，Prefabを追加

---
+++ Assets/MusicSelection/Scripts/MusicUIElement.cs
@@ -0,0 +1,34 @@
+using System;
+using Rhythm;
+using UnityEngine;
+using Transition;
+
+namespace MusicSelection
+{
+    public class MusicUIElement : MonoBehaviour
+    {
+        private RhythmId _rhythmId;
+        private BeatmapInformation _beatmapInfo;
+
+        private static GameObject _prefab;
+        private static GameObject _parent;
+
+        public void Init(BeatmapInformation info)
+        {
+            _beatmapInfo = info;
+            _rhythmId = (RhythmId)Enum.Parse(typeof(RhythmId), _beatmapInfo.Id);
+        }
+
+        public void OnSubmit()
+        {
+            SceneTransitionManager.TransitionToRhythm(_rhythmId, false);
+        }
+
+        public void OnCancel()
+        {
+            // モードセレクトSceneへ
+            // SceneTransitionManager.TransitionTo();
+            Debug.Log("OnCancel()が実行されました");
+        }
+    }
+}
