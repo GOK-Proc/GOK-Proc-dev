2ae933575430df80b9b4e9a7b2e78b6a8b8f089d
Ryotan1ff <ryotan0511@gmail.com>
2024-08-11 22:59:29 +0900

    string -> RhythmId の変換で例外をキャッチしないように
    RhythmIdの自動生成が実装され，例外の危険度が上がったためcatchで例外隠しをするわけにいかなくなった．

--- Assets/MusicSelection/Scripts/MusicUIElement.cs
+++ Assets/MusicSelection/Scripts/MusicUIElement.cs
@@ -19,16 +19,7 @@ namespace MusicSelection
         public void Init(BeatmapInformation info, Thumbnail thumbnail)
         {
             _beatmapInfo = info;
-            try
-            {
-                // TODO:
-                // issue#59 RhythmIdの自動生成ができていないとArgumentException
-                _rhythmId = (RhythmId)Enum.Parse(typeof(RhythmId), _beatmapInfo.Id);
-            }
-            catch
-            {
-                // Do nothing
-            }
+            _rhythmId = (RhythmId)Enum.Parse(typeof(RhythmId), _beatmapInfo.Id);
 
             _thumbnail = thumbnail;
             _text = GetComponent<TextMeshProUGUI>();
