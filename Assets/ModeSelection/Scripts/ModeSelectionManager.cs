b2e5af997dc04ac87305f73f8e9890d89a3279c7
Ryotan1ff <ryotan0511@gmail.com>
2024-08-19 18:01:59 +0900

    バナーに，選択して決定ボタンを押すと次のシーンへ遷移する機能を入れた
    TransitionMangerの実装が済んでいないためまだ仮

---
+++ Assets/ModeSelection/Scripts/ModeSelectionManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using Transition;
+
+namespace ModeSelection
+{
+    public class ModeSelectionManager: MonoBehaviour
+    {
+        public void ToMap()
+        {
+            SceneTransitionManager.TransitionToMap();
+        }
+        
+        public void ToMusicSelection()
+        {
+            Debug.Log("ここで楽曲選択シーンへ遷移します");
+            // TODO: 
+            // SceneTransitionManager.TransitionToSelection();
+        }
+
+        public void ToGallery()
+        {
+            Debug.Log("ギャラリーシーンへ遷移します");
+            // TODO: 
+            // SceneTransitionManager.TransitionToGallery();
+        }
+    }
+}
