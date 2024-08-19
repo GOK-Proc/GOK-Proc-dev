e631fa27eeaefee258d6bb9a04601f19acc3f073
Ryotan1ff <ryotan0511@gmail.com>
2024-08-19 18:09:13 +0900

    モードセレクトから各モードへの遷移を記述

--- Assets/ModeSelection/Scripts/ModeSelectionManager.cs
+++ Assets/ModeSelection/Scripts/ModeSelectionManager.cs
@@ -3,25 +3,21 @@ using Transition;
 
 namespace ModeSelection
 {
-    public class ModeSelectionManager: MonoBehaviour
+    public class ModeSelectionManager : MonoBehaviour
     {
         public void ToMap()
         {
             SceneTransitionManager.TransitionToMap();
         }
-        
+
         public void ToMusicSelection()
         {
-            Debug.Log("ここで楽曲選択シーンへ遷移します");
-            // TODO: 
-            // SceneTransitionManager.TransitionToSelection();
+            SceneTransitionManager.TransitionToMusicSelection();
         }
 
         public void ToGallery()
         {
-            Debug.Log("ギャラリーシーンへ遷移します");
-            // TODO: 
-            // SceneTransitionManager.TransitionToGallery();
+            SceneTransitionManager.TransitionToGallery();
         }
     }
 }
