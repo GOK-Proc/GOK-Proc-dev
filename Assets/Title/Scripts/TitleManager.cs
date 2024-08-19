28c16594c5f5b7f64f068b6608e37267f99a2522
Ryotan1ff <ryotan0511@gmail.com>
2024-08-19 12:15:12 +0900

    TitleManagerクラスの追加

---
+++ Assets/Title/Scripts/TitleManager.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using KanKikuchi.AudioManager;
+
+namespace Title
+{
+    public class TitleManager : MonoBehaviour
+    {
+        void Start()
+        {
+            // TODO: BGM再生
+            // BGMManager.Instance.Play("titleBGMPath");
+        }
+    }
+}
