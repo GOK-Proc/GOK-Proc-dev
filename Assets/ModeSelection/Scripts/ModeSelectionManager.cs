66db82e924cceb4bcbc54b9276bfef44e7d6b702
Ryotan1ff <ryotan0511@gmail.com>
2024-08-19 18:15:38 +0900

    BGM再生部分の仮実装

--- Assets/ModeSelection/Scripts/ModeSelectionManager.cs
+++ Assets/ModeSelection/Scripts/ModeSelectionManager.cs
@@ -1,10 +1,17 @@
 ﻿using UnityEngine;
+using KanKikuchi.AudioManager;
 using Transition;
 
 namespace ModeSelection
 {
     public class ModeSelectionManager : MonoBehaviour
     {
+        private void Start()
+        {
+            // TODO: BGM再生
+            // BGMManager.Instance.Play("path");
+        }
+        
         public void ToMap()
         {
             SceneTransitionManager.TransitionToMap();
