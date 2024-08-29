a19daba07ce2a73ee64971ef51a191adfb5715b4
Ryotan1ff <ryotan0511@gmail.com>
2024-08-29 15:17:02 +0900

    左右ボタンでページ送りできる説明文を実装

---
+++ Assets/Gallery/Scripts/Description.cs
@@ -0,0 +1,51 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Gallery
+{
+    [RequireComponent(typeof(TextMeshProUGUI))]
+    [RequireComponent(typeof(PlayerInput))]
+    public class Description : MonoBehaviour
+    {
+        private TextMeshProUGUI _descriptionText;
+
+        private void Awake()
+        {
+            _descriptionText = GetComponent<TextMeshProUGUI>();
+        }
+
+        public void OnNavigateHorizontal(InputValue inputValue)
+        {
+            var inputHorizontal = inputValue.Get<Vector2>().x;
+            Debug.Log("horizontal");
+
+            switch (inputHorizontal)
+            {
+                case > 0f:
+                    ShowNextPage();
+                    break;
+                case < 0f:
+                    ShowPreviousPage();
+                    break;
+            }
+        }
+
+        private void ShowNextPage()
+        {
+            var pageIsLast = _descriptionText.pageToDisplay >= _descriptionText.textInfo.pageCount;
+            if (pageIsLast) return;
+            
+            _descriptionText.pageToDisplay++;
+        }
+
+        private void ShowPreviousPage()
+        {
+            // pageは1-indexed
+            var pageIsFirst = _descriptionText.pageToDisplay <= 1;
+            if (pageIsFirst) return;
+            
+            _descriptionText.pageToDisplay--;
+        }
+    }
+}
