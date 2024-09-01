ae276d4ae118353467af468da874479a44af0223
Ryotan1ff <ryotan0511@gmail.com>
2024-09-01 23:01:18 +0900

    ドットでページ数を示すUI要素を記述

---
+++ Assets/Gallery/Scripts/Dot.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Pool;
+
+namespace Gallery
+{
+    public class Dot : Image
+    {
+        private IObjectPool<Dot> _objectPool;
+
+        public IObjectPool<Dot> ObjectPool
+        {
+            set => _objectPool = value;
+        }
+    };
+}
