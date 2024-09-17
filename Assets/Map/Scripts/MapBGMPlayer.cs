68a2f4fb8a7095a11c3d85744d10fa61f7beab66
kamekiti <ishi.y0331kamekiti@gmail.com>
2024-09-17 16:24:58 +0900

    MapシーンでBGMを再生するように変更

---
+++ Assets/Map/Scripts/MapBGMPlayer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using KanKikuchi.AudioManager;
+
+namespace Map
+{
+	class BGMPlayer : MonoBehaviour
+	{
+		private void Start()
+		{
+			BGMManager.Instance.Play(BGMPath.MAP_INTRO, BGMPath.MAP_LOOP);
+		}
+	}
+}
