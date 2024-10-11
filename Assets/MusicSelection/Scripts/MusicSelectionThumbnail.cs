933a3cefa654b21c34b58f6088517e8d51c9e3af
Ryotan1ff <ryotan0511@gmail.com>
2024-10-11 12:32:18 +0900

    楽曲選択に，譜面制作者名とアチーブメントの表示を追加

--- Assets/MusicSelection/Scripts/MusicSelectionThumbnail.cs
+++ Assets/MusicSelection/Scripts/MusicSelectionThumbnail.cs
@@ -7,30 +7,48 @@ namespace MusicSelection
 {
     public class MusicSelectionThumbnail : ThumbnailBase
     {
-        private TrackInformation _trackCache;
-        
+        private TrackInformation _track;
+        private NotesInformation[] _notes;
+
+        [SerializeField] private TextMeshProUGUI _notesDesignerText;
+
         [SerializeField] private TextMeshProUGUI _bestScoreText;
         [SerializeField] private TextMeshProUGUI _maxComboText;
 
+        [SerializeField] private TextMeshProUGUI _allPerfectText;
+        [SerializeField] private TextMeshProUGUI _fullComboText;
+
         [SerializeField] private RecordList _recordList;
+        [SerializeField] private BeatmapData _beatmapData;
 
         public override void Set(TrackInformation track)
         {
-            _trackCache = track;
-            
+            _track = track;
             _image.sprite = track.Thumbnail;
-
             _titleText.text = track.Title;
             _composerText.text = track.Composer;
+
+            _notes = _beatmapData.BeatmapDictionary[track.Id].Notes;
 
             Set(DifficultySelection.Current);
         }
 
         public void Set(Difficulty difficulty)
         {
-            var record = _recordList[_trackCache.Id][(int)difficulty];
+            var record = _recordList[_track.Id][(int)difficulty];
             _bestScoreText.text = $"{record.Score.ToString()} pt";
             _maxComboText.text = $"{record.MaxCombo.ToString()} combo";
+
+            _allPerfectText.gameObject.SetActive(record.Achievement switch
+            {
+                Achievement.AllPerfect => true, _ => false
+            });
+            _fullComboText.gameObject.SetActive(record.Achievement switch
+            {
+                Achievement.FullCombo => true, _ => false
+            });
+
+            _notesDesignerText.text = _notes[(int)difficulty].NotesDesigner;
         }
     }
 }
