8c614dfda57c5f8c808d2a1b72350a774d6a3802
kamekiti <ishi.y0331kamekiti@gmail.com>
2024-10-31 18:34:10 +0900

    タイトル画面にバージョン情報を表示するように変更

---
+++ Assets/Title/Scripts/DisplayVersion.cs
@@ -0,0 +1,16 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Title
+{
+    public class DisplayVersion : MonoBehaviour
+    {
+        private TextMeshProUGUI textMeshProUGUI;    
+
+        void Start()
+        {
+            textMeshProUGUI = GetComponent<TextMeshProUGUI>();
+            textMeshProUGUI.text = $"Ver. {Application.version}";
+        }
+    }
+}
