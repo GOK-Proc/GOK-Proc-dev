5cd27630c6e1f55582c128510ccdfd579ee09433
kamekiti <ishi.y0331kamekiti@gmail.com>
2024-11-27 01:34:38 +0900

    設定項目のデータ構造を定義

---
+++ Assets/Settings/Scripts/FrameRate.cs
@@ -0,0 +1,13 @@
+﻿namespace Settings
+{
+	public enum FrameRate
+	{
+		VSync,
+		Fps30,
+		Fps60,
+		Fps120,
+		Fps144,
+		Fps240,
+		Unlimited
+	}
+}
---
+++ Assets/Settings/Scripts/UserSettings.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace Settings
+{
+	[CreateAssetMenu]
+	public class UserSettings : EncryptedScriptableObject
+	{
+		[Header("Audio Settings")]
+
+		[Range(0, 10)]
+		[SerializeField] private int _bgmVolume = 5;
+
+		[Range(0, 10)]
+		[SerializeField] private int _soundEffectVolume = 5;
+
+		[Range(0, 10)]
+		[SerializeField] private int _musicVolume = 5;
+
+		[Range(0, 10)]
+		[SerializeField] private int _rhythmEffectVolume = 5;
+
+		[Header("Play Settings")]
+
+		[SerializeField] private KeyConfigId _keyConfigId;
+
+		[Range(0.5f, 3.0f)]
+		[SerializeField] private float _highSpeed = 1f;
+
+		[Header("General Settings")]
+
+		[SerializeField] private ScreenMode _screenMode;
+
+		[SerializeField] private FrameRate _frameRate;
+
+		[SerializeField] private ScenarioDifficulty _scenarioDifficulty;
+	}
+}
