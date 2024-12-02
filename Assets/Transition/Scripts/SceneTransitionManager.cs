998ff59e4bb029794fdd9630e6eea709e632ec78
kamekiti <ishi.y0331kamekiti@gmail.com>
2024-12-02 18:04:55 +0900

    ノベルシーンとの間の遷移時にSEManagerの音量を切り替えるように変更

--- Assets/Transition/Scripts/SceneTransitionManager.cs
+++ Assets/Transition/Scripts/SceneTransitionManager.cs
@@ -23,8 +23,14 @@ namespace Transition
 
 		private static Scene _prevScene;
 
+		private static EpisodeFlagManager _episodeFlagManager;
+		private static SettingsManager _settingsManager;
+
 		private void OnEnable()
 		{
+			_episodeFlagManager = GameObject.FindWithTag("EpisodeFlagManager").GetComponent<EpisodeFlagManager>();
+			_settingsManager = GameObject.FindWithTag("SettingsManager").GetComponent<SettingsManager>();
+
 			_overlay = GameObject.FindWithTag("TransitionOverlay").GetComponent<CanvasGroup>();
 
 			SceneManager.LoadScene(SceneName.Title.ToString(), LoadSceneMode.Additive);
@@ -47,6 +53,8 @@ namespace Transition
 
 		public static void TransitionToMap()
 		{
+			_settingsManager.SetDefaultSeVolume();
+		
 			TransitionToScene(SceneName.Map);
 		}
 
@@ -54,15 +62,13 @@ namespace Transition
 		{
 			if (result)
 			{
-				EpisodeFlagManager episodeFlagManager = GameObject.FindWithTag("EpisodeFlagManager").GetComponent<EpisodeFlagManager>();
-
 				switch (CurrentEpisodeType)
 				{
 					case EpisodeType.Novel:
-						episodeFlagManager.SetNextFlag(CurrentNovelId);
+						_episodeFlagManager.SetNextFlag(CurrentNovelId);
 						break;
 					case EpisodeType.Rhythm:
-						episodeFlagManager.SetNextFlag(CurrentRhythmId);
+						_episodeFlagManager.SetNextFlag(CurrentRhythmId);
 						break;
 					default:
 						return;
@@ -89,6 +95,8 @@ namespace Transition
 		public static void TransitionToNovel(NovelId novelId)
 		{
 			if (novelId == NovelId.None) return;
+
+			_settingsManager.SetNovelSeVolume();
 
 			CurrentEpisodeType = EpisodeType.Novel;
 			CurrentNovelId = novelId;
