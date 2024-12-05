755971d1b176ed249590e9e0461f2fc6af8bd947
kamekiti <ishi.y0331kamekiti@gmail.com>
2024-12-05 17:41:15 +0900

    設定シーンでBGMを再生するように変更

---
+++ Assets/Settings/Scripts/SettingsManager.cs
@@ -0,0 +1,13 @@
+﻿using KanKikuchi.AudioManager;
+using UnityEngine;
+
+namespace Settings
+{
+	public class SettingsManager : MonoBehaviour
+	{
+		private void Start()
+		{
+			BGMManager.Instance.Play(BGMPath.SETTING);
+		}
+	}
+}
