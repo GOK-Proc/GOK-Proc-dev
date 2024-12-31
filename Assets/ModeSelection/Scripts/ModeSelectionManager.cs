f56872b49f55dde2bd492b4dbd3e01f7b9a71c29
kamekiti <ishi.y0331kamekiti@gmail.com>
2024-12-31 12:53:05 +0900

    バナー画像を更新

--- Assets/ModeSelection/Scripts/ModeSelectionManager.cs
+++ Assets/ModeSelection/Scripts/ModeSelectionManager.cs
@@ -5,45 +5,51 @@ using Transition;
 
 namespace ModeSelection
 {
-    public class ModeSelectionManager : MonoBehaviour
-    {
-        [SerializeField] private EventSystem _eventSystem;
-        [SerializeField] private GameObject[] _selectableBanners;
+	public class ModeSelectionManager : MonoBehaviour
+	{
+		[SerializeField] private EventSystem _eventSystem;
+		[SerializeField] private GameObject[] _selectableBanners;
 
-        private void Awake()
-        {
-            _eventSystem.firstSelectedGameObject = SceneTransitionManager.RecentSceneName switch
-            {
-                SceneName.Title or SceneName.Map => _selectableBanners[0],
-                SceneName.MusicSelection => _selectableBanners[1],
-                SceneName.Gallery => _selectableBanners[2],
-                _ => _selectableBanners[0]
-            };
-        }
+		private void Awake()
+		{
+			_eventSystem.firstSelectedGameObject = SceneTransitionManager.RecentSceneName switch
+			{
+				SceneName.Title or SceneName.Rhythm => _selectableBanners[0],
+				SceneName.Map => _selectableBanners[1],
+				SceneName.Gallery => _selectableBanners[2],
+				SceneName.Settings => _selectableBanners[3],
+				_ => _selectableBanners[0]
+			};
+		}
 
-        private void Start()
-        {
-            BGMManager.Instance.Play(BGMPath.MODE_INTRO, BGMPath.MODE_LOOP);
-        }
+		private void Start()
+		{
+			BGMManager.Instance.Play(BGMPath.MODE_INTRO, BGMPath.MODE_LOOP);
+		}
 
-        public void ToTitle()
-        {
-            SceneTransitionManager.TransitionToTitle();
-        }
+		public void ToTitle()
+		{
+			SceneTransitionManager.TransitionToTitle();
+		}
 
-        public void ToMap()
-        {
-            SceneTransitionManager.TransitionToMap();
-        }
+		public void ToMap()
+		{
+			SceneTransitionManager.TransitionToMap();
+		}
 
-        public void ToMusicSelection()
-        {
-            SceneTransitionManager.TransitionToMusicSelection();
-        }
+		public void ToMusicSelection()
+		{
+			SceneTransitionManager.TransitionToMusicSelection();
+		}
 
-        public void ToGallery()
-        {
-            SceneTransitionManager.TransitionToGallery();
-        }
-    }
+		public void ToGallery()
+		{
+			SceneTransitionManager.TransitionToGallery();
+		}
+
+		public void ToSettings()
+		{
+			SceneTransitionManager.TransitionToSettings();
+		}
+	}
 }
