6d98b4c660ea79466df8c902b3ddb522416e85b3
DESKTOP-RMQ5C3B\tokas <tkrshun0408@gmail.com>
2025-01-14 23:33:48 +0900

    スライダーを作成

--- Assets/Adjustment/Scripts/SliderValueText.cs
+++ Assets/Adjustment/Scripts/SliderValueText.cs
@@ -1,18 +1,29 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class SliderValueText : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Transform _sliderHandle;
+    private TextMeshProUGUI _text;
+
+    private void Awake()
     {
-        
+        _text = GetComponent<TextMeshProUGUI>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnValueChanged(float value)
     {
-        
+        _text.text = value switch
+        {
+            > 0 => $"＋{value}",
+            < 0 => $"－{-value}",
+            _ => $"{value}"
+        };
+
+        var pos = transform.localPosition;
+        pos.x = _sliderHandle.localPosition.x;
+        transform.localPosition = pos;
     }
 }
